dfb3b4740b49d0489f71294c11d359542273fa81
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-03-10 18:51:29 +0100

    feat(auth): add getUser function to fetch user data from API

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,5 +1,9 @@
-import { API_URL } from "../utils";
+import { API_URL, getOptionsApi } from "../utils";
 
 export const initSession = async () => {
   await fetch(`${API_URL}/sanctum/csrf-cookie`);
+};
+
+export const getUser = async (): Promise<Response> => {
+  return await fetch(`${API_URL}/api/user`, getOptionsApi());
 };
