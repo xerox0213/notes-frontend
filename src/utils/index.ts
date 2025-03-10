443295f6e8748a2c910d81e6f7fbd72df32beacc
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-03-10 18:50:33 +0100

    feat(utils): add getOptionsApi function for API request options

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,15 @@
+import Cookies from "js-cookie";
+
 export const API_URL = import.meta.env.API_URL;
+
+export const getOptionsApi = (method: string = "GET"): RequestInit => {
+  return {
+    method,
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-XSRF-TOKEN": Cookies.get("X-XSRF-TOKEN") ?? "",
+    },
+    credentials: "include",
+  };
+};
