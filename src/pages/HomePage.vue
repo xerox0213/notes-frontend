12a6c405b578ef28c841c951bdc07da2022b2ef6
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-03-13 21:01:32 +0100

    feat(home): add HomePage component to display user information

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import { useUserStore } from "../stores/userStore";
+
+  const userStore = useUserStore();
+</script>
+
+<template>
+    <pre>
+        {{ userStore.user }}
+    </pre>
+</template>
