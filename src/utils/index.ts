ca8a18918d8837a8df64f74f4cc129c95cff3453
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-03-13 19:53:00 +0100

    fix(utils): update API_URL and refactor options function for improved request handling

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,16 @@
 import Cookies from "js-cookie";
 
-export const API_URL = import.meta.env.API_URL;
+export const API_URL = import.meta.env.VITE_API_URL;
 
-export const getOptionsApi = (method: string = "GET"): RequestInit => {
+export const options = (method?: string, body?: object | []): RequestInit => {
   return {
     method,
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-      "X-XSRF-TOKEN": Cookies.get("X-XSRF-TOKEN") ?? "",
+      "X-XSRF-TOKEN": Cookies.get("XSRF-TOKEN") ?? "",
     },
+    body: JSON.stringify(body),
     credentials: "include",
   };
 };
