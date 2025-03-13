888b6736a0156af2738b820b6db94ebd670c34c2
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-03-13 19:57:15 +0100

    feat(auth): add register function api

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,9 +1,29 @@
-import { API_URL, getOptionsApi } from "../utils";
+import { CsrfMismatchException, RegistrationException } from "../exeptions";
+import type { RegistrationSchema } from "../pages/RegisterPage.vue";
+import { API_URL, options } from "../utils";
 
 export const initSession = async () => {
-  await fetch(`${API_URL}/sanctum/csrf-cookie`);
+  await fetch(`${API_URL}/sanctum/csrf-cookie`, options());
 };
 
 export const getUser = async (): Promise<Response> => {
-  return await fetch(`${API_URL}/api/user`, getOptionsApi());
+  return await fetch(`${API_URL}/api/user`, options("GET"));
+};
+
+export const register = async (credentials: RegistrationSchema) => {
+  await initSession();
+
+  const res = await fetch(
+    `${API_URL}/api/register`,
+    options("POST", credentials),
+  );
+
+  if (!res.ok) {
+    switch (res.status) {
+      case 419:
+        return Promise.reject(new CsrfMismatchException(await res.json()));
+      case 422:
+        return Promise.reject(new RegistrationException(await res.json()));
+    }
+  }
 };
