76d9f99555e2db1bb5ac522fea546706b9452318
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-03-13 19:56:00 +0100

    feat(types): add registration error types for improved validation handling

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,15 @@
+import type { RegistrationSchema } from "../pages/RegisterPage.vue";
+
 export interface User {
   first_name: string;
   last_name: string;
   email: string;
 }
+
+export type BasicError = { message: string };
+
+export type RegistrationError = BasicError & {
+  errors: Partial<{
+    [K in keyof RegistrationSchema]: string[];
+  }>;
+};
